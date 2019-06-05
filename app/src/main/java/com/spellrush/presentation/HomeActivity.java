e6609bfd84609e11b0f6b26069e256804122314f
mike <winkler1@myumanitoba.ca>
2019-06-05 10:35:40 -0500

    Added buttons to main screen with temp toast messages

--- app/src/main/java/com/spellrush/presentation/HomeActivity.java
+++ app/src/main/java/com/spellrush/presentation/HomeActivity.java
@@ -5,9 +5,12 @@ import com.spellrush.application.Main;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
 
@@ -15,6 +18,30 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button startButton = (Button) findViewById(R.id.startBtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Start Button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button settingsButton = (Button) findViewById(R.id.settingsBtn);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Settings Button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button leaderBoardButton = (Button) findViewById(R.id.leaderBoardBtn);
+        leaderBoardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Leader Board Button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
