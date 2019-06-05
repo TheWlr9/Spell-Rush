fe3152477a3f474a4ff91c7544a3f6ad118c1125
Chad Hillary <chadhillary2013@gmail.com>
2019-06-04 20:47:07 -0500

    Test using a surface view for game UI

--- app/src/main/java/com/spellrush/presentation/HomeActivity.java
+++ app/src/main/java/com/spellrush/presentation/HomeActivity.java
@@ -7,35 +7,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 public class HomeActivity extends Activity {
+
+    GameView gameView;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        this.surfaceView = (SurfaceView) findViewById(R.id.gameView);
+        this.surfaceHolder = surfaceView.getHolder();
+
+        surfaceHolder.addCallback(gameView);
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
     }
 }
