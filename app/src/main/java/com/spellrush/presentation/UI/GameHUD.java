f560513f71034a96e6db34119abef54ede5856c9
Chad Hillary <hillaryc@myumanitoba.ca>
2019-06-12 22:04:01 -0500

    Revert "update gitignore, gradle stuff"
    This reverts commit 52deca11fee8ea93d52bacf916036c64a5f818e9.

--- app/src/main/java/com/spellrush/presentation/UI/GameHUD.java
+++ app/src/main/java/com/spellrush/presentation/UI/GameHUD.java
@@ -1,32 +1,41 @@
 package com.spellrush.presentation.UI;
 
+import android.content.res.Resources;
 import android.graphics.Canvas;
 
+import com.spellrush.business.GameView;
 import com.spellrush.objects.GameObject;
 import com.spellrush.business.PlayerController;
 import com.spellrush.presentation.UI.Components.HealthBar;
+import com.spellrush.presentation.UI.Components.ScoreDisplay;
 
-public class GameHUD extends GameObject
-{
+public class GameHUD extends GameObject {
     private static final int HUD_DEPTH = -100;
-    private PlayerController player; //todo: Move Player somewhere logical, and pass in HP
     private HealthBar hpUI;
+    private ScoreDisplay score;
+    private PlayerController player;
 
-    public GameHUD(){
+    public GameHUD(PlayerController player) {
         super(HUD_DEPTH);
-        player = new PlayerController();
-        hpUI = new HealthBar(20,100,900,50, 15);
+        this.player = player;
+
+        int deviceWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int deviceHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+        hpUI = new HealthBar(50,deviceHeight - 100,deviceWidth - 75,50, 15);
+        score = new ScoreDisplay(deviceWidth - 475, deviceHeight - 130);
     } // end constructor method
 
     @Override
     public void update(){
-        player.update();
+        // Nothing to do yet.
     } // end update()
 
     @Override
     public void draw(Canvas canvas){
         if(canvas != null) {
             hpUI.drawHealthBar(canvas, player.MAX_HP, player.getHP());
+            score.drawScore(canvas, player.getScore());
         }
     } // end draw()
 } // end GameHudView class
