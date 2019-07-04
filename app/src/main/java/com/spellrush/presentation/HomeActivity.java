0a8e3611417c01c15ce4ae8df30eafba9aa9f0ec
Earl <placidej@myumanitoba.ca>
2019-07-03 21:24:49 -0500

    Hotfix - Move database copier to a utils helper class
    - also added an assert to our dao tests

--- app/src/main/java/com/spellrush/presentation/HomeActivity.java
+++ app/src/main/java/com/spellrush/presentation/HomeActivity.java
@@ -12,14 +12,7 @@ import android.widget.Toast;
 import com.spellrush.R;
 import com.spellrush.audio.AudioManager;
 import com.spellrush.audio.SoundEvent;
-import com.spellrush.services.Services;
-
-import java.sql.SQLOutput;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.spellrush.persistence.utils.DBHelper;
 
 public class HomeActivity extends Activity {
     final static int[] HOME_SOUND_RES_IDS = {R.raw.lines_of_code};
@@ -34,7 +27,7 @@ public class HomeActivity extends Activity {
         createStartButton();
         //createSettingsButton();
         createLeaderboardButton();
-        copyDatabaseToDevice();
+        DBHelper.copyDatabaseToDevice(getApplicationContext());
     }
 
     @Override
@@ -96,60 +89,5 @@ public class HomeActivity extends Activity {
     @Override
     public void onBackPressed() {
         // Ensure it does nothing...
-    }
-
-    // FROM SAMPLE PROJECT - Copies database to the device
-    private void copyDatabaseToDevice() {
-        final String DB_PATH = "db";
-
-        String[] assetNames;
-        Context context = getApplicationContext();
-        File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
-        AssetManager assetManager = getAssets();
-
-        try {
-            assetNames = assetManager.list(DB_PATH);
-            for (int i = 0; i < assetNames.length; i++) {
-                assetNames[i] = DB_PATH + "/" + assetNames[i];
-            }
-
-            copyAssetsToDirectory(assetNames, dataDirectory);
-
-            Services.setDBPathName(dataDirectory.toString() + "/" + Services.getDBPathName());
-
-            System.out.println("Database Path: " + Services.getDBPathName());
-
-        } catch (final IOException ioe) {
-            throw new RuntimeException(ioe);
-        }
-    }
-
-    // FROM SAMPLE PROJECT - goes through each database asset and copies into device
-    private void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
-        AssetManager assetManager = getAssets();
-
-        for (String asset : assets) {
-            String[] components = asset.split("/");
-            String copyPath = directory.toString() + "/" + components[components.length - 1];
-
-            char[] buffer = new char[1024];
-            int count;
-
-            File outFile = new File(copyPath);
-
-            if (!outFile.exists()) {
-                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
-                FileWriter out = new FileWriter(outFile);
-
-                count = in.read(buffer);
-                while (count != -1) {
-                    out.write(buffer, 0, count);
-                    count = in.read(buffer);
-                }
-
-                out.close();
-                in.close();
-            }
-        }
     }
 }
--- app/src/test/java/com/spellrush/application/ScoreEntryTests.java
+++ app/src/test/java/com/spellrush/application/ScoreEntryTests.java
@@ -6,13 +6,16 @@ import static org.junit.Assert.*;
 public class ScoreEntryTests {
     @Test
     public void verifyScoreIsCreated() {
-        ScoreEntry scoreEntry;
+        ScoreEntry scoreEntry1, scoreEntry2;
 
         System.out.println("\nStarting Test - ScoreEntry - Verify that a scoreEntry instance is created");
-        scoreEntry = new ScoreEntry("Earl", 500);
-        assertNotNull(scoreEntry);
-        assertTrue("Earl".equals(scoreEntry.getPlayerName()));
-        assertTrue(500 == scoreEntry.getPlayerScore());
+        scoreEntry1 = new ScoreEntry("Earl", 500);
+        assertNotNull(scoreEntry1);
+        assertTrue("Earl".equals(scoreEntry1.getPlayerName()));
+        assertTrue(500 == scoreEntry1.getPlayerScore());
+
+        scoreEntry2 = new ScoreEntry("Joe", 501);
+        assertTrue(scoreEntry2.compareTo(scoreEntry1) < 0);
 
         System.out.println("Finished Test - ScoreEntry - Verify that a scoreEntry instance is created");
     } //end verifyScoreIsCreated()
