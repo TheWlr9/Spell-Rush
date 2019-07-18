6004b7a05a94db81e22ed63af430d1caeaefdd7a
mike <winkler1@myumanitoba.ca>
2019-07-18 14:41:30 -0500

    added dots at the bottom of the TUT screens

--- app/src/main/java/com/spellrush/presentation/TutorialActivity.java
+++ app/src/main/java/com/spellrush/presentation/TutorialActivity.java
@@ -2,7 +2,10 @@ package com.spellrush.presentation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.spellrush.R;
 
@@ -14,9 +17,44 @@ public class TutorialActivity extends Activity {
         setContentView(R.layout.activity_tutorial);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.tutorial);
+        LinearLayout sliderDotsPanel = (LinearLayout)findViewById(R.id.tutDots);
         TutorialAdapter adapter= new TutorialAdapter(this);
         viewPager.setAdapter(adapter);
 
+        final int dotsCount= adapter.getCount();
+        final ImageView[] dots = new ImageView[dotsCount];
+
+        for(int i = 0 ; i<dotsCount; i++){
+            dots[i]=new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.non_active_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8,0,8,0);
+
+            sliderDotsPanel.addView(dots[i], params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i<dotsCount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
+                }
+                    dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
 }
