314805f36e5fa7970c6ec7cfad04364bb1c02d20
William <ritchi14@myumanitoba.ca>
2019-07-21 18:40:10 -0500

    Added the PC animation

--- app/src/main/java/com/spellrush/business/PlayerController.java
+++ app/src/main/java/com/spellrush/business/PlayerController.java
@@ -1,7 +1,10 @@
 package com.spellrush.business;
 
+import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 
+import com.spellrush.R;
 import com.spellrush.objects.HealthObject;
 
 /*******************************************
@@ -12,15 +15,27 @@ import com.spellrush.objects.HealthObject;
  * *****************************************/
 public class PlayerController extends HealthObject {
 
+    public static final int Y_POSITION = 1650;
+    public static final int SPRITE_WIDTH = 360;
+    public static final int SPRITE_HEIGHT = 328;
+
+    private static final int ANIMATION_SPEED = GameThread.FRAMES_PER_SECOND / 2;
+
     // Follow Singleton design pattern
     private static final PlayerController instance = new PlayerController();
 
     public static final int MAX_HP = 30;
     private int score;
+    private int drawTimer;
+    private boolean frame;
+    private Drawable spriteFrame1;
+    private Drawable spriteFrame2;
+    private Drawable currSprite = null;
 
     private PlayerController() {
         super(0, MAX_HP);
         score = 0;
+        drawTimer = 0;
     }
 
     public static PlayerController getInstance(){
@@ -47,12 +62,36 @@ public class PlayerController extends HealthObject {
         score = 0;
     }
 
-    @Override
-    public void update(){
-        // addScore(1);
+    private void initSprite(){
+        Resources r = GameView.getInstance().getContext().getResources();
+        spriteFrame1 = r.getDrawable(R.drawable.player2);
+        spriteFrame2 = r.getDrawable(R.drawable.player1);
+        int spriteX = Resources.getSystem().getDisplayMetrics().widthPixels / 2;
+        spriteFrame1.setBounds(spriteX - (SPRITE_WIDTH / 2), Y_POSITION - (SPRITE_HEIGHT / 2),
+                spriteX + (SPRITE_WIDTH / 2), Y_POSITION + (SPRITE_HEIGHT / 2));
+        spriteFrame2.setBounds(spriteFrame1.getBounds());
+        currSprite = spriteFrame1;
+    }
+
+    private int updateDrawTimer(int drawTime){
+        drawTime++;
+        if(drawTime > ANIMATION_SPEED){
+            drawTime = 0;
+            frame = !frame;
+            currSprite = frame ? spriteFrame1 : spriteFrame2;
+        }
+        return drawTime;
     }
 
     @Override
-    public void draw(Canvas canvas) { return; }
+    public void update(){
+        drawTimer = updateDrawTimer(drawTimer);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        if(currSprite==null){initSprite();}
+        currSprite.draw(canvas);
+    }
 
 } // end PlayerController class
